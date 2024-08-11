f88306da26de0530ae929fa3411b742949511e26
Tanvir <tanvir@Tanvirs-MacBook-Air.local>
2024-08-11 10:15:43 +0600

    useCallback Hook added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useContext } from 'react'
+import { useCallback, useContext, useState } from 'react'
 import Home from './components/Home/Home'
 import { Context1Provider, NameContext } from './contexts/context1'
 import './App.css'
@@ -8,13 +8,22 @@ import useCounter from './hooks/useCounter';
 import Counter1 from './components/Counter1/Counter1';
 import Counter2 from './components/Counter2/Counter2';
 import UseMemoHook from './components/UseMemoHook/UseMemoHook';
+import UseCallBack from './components/UseCallBack/UseCallBack';
 
 
 function App() {
+  const [increase, setIncrease]=useState(0)
+  const [deccrease, setDecrease]=useState(50)
+  const testFunForUseCallback=useCallback(()=>{
+    console.log("I am call back")
+
+  },[])
 
   return (
     <div>
-         <UseMemoHook/>
+         <UseCallBack testFunForUseCallback={testFunForUseCallback}/>
+         <button onClick={()=> setIncrease(increase+1)}>Increase- {increase}</button>
+         <button onClick={()=> setDecrease(deccrease-1)}>Decrease- {deccrease}</button>
     </div>
   )
 }
---
+++ src/components/UseCallBack/UseCallBack.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const UseCallBack=({testFunForUseCallback})=>{
+
+    console.log("Call back calling...")
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default React.memo(UseCallBack);
